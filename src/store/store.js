bcefeb3395f7b5c7c68a484491b98172e6b9799c
Colin Beltz <colinbeltz@gmail.com>
2023-08-08 22:25:10 -0500

    Project file setup

---
+++ src/store/store.js
@@ -0,0 +1,17 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { cartReducer } from "./cartSlice";
+
+export const store = configureStore({
+    reducer: {
+        cartState: cartReducer,
+    }
+});
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import { cartReducer } from "./cartSlice";
+
+// export const store = configureStore({
+//     reducer: {
+//         cartState: cartReducer,
+//     }
+// });
